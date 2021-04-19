26deb878a9e91356b1c4fc2d6af12c286c1a2870
Doyinsola24 <62024110+Doyinsola24@users.noreply.github.com>
2021-04-20 00:00:30 +0100

    changes updated

--- filesystem.js
+++ filesystem.js
@@ -1,6 +1,11 @@
 var http = require('http')
 var fs = require('fs')
 
+
+fs.mkdir('./result', {recursive: true}, (err) => {
+    if (err) throw err;
+})
+
 http.get("http://jsonplaceholder.typicode.com/posts", function (res){
     var data = "";
     res.on('data', function (chunk) {
